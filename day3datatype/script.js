482cc26734ba316d420fc35006951964cab7b27e
shubham <shubhampandeyup07@gmail.com>
2023-10-14 09:07:59 +0530

    declared the variables

---
+++ day3datatype/script.js
@@ -0,0 +1,19 @@
+// let text = "ram"
+// let name = 'Ram'
+// let text2 = ` your namejkkkdsoifalkfjl; %##2662322%3 ${name}`
+
+// console.log(text2)
+
+// let a = 43636363636
+// console.log(a)
+
+
+// const number = 3/0;
+// console.log(number)   //infinity
+
+
+// let name;
+// console.log(name);    //undefined
+
+
+
