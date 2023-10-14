78542f6f2fe04de091399569c2af94bec7d28ea6
shubham <shubhampandeyup07@gmail.com>
2023-10-14 09:19:54 +0530

    for loop codes , use directely in js files

---
+++ day7forLoop/script.js
@@ -0,0 +1,104 @@
+// for(i=1;i<=10;i++){
+//     console.log(2*i)
+// }
+
+// for(i=1;i<=10;i++) {
+//      console.log(2*i)
+// }
+
+
+
+// let message ="welcome to primathon"
+// for(i=1;i<10;i++){
+//     console.log(message)
+// }
+
+
+// let message ="welcome to primathon"
+// for(i=1;i<5;i++)
+//     console.log(message)
+//     console.log("Loop terminated")
+
+
+// const n=5;
+// for(i=1; i<=n; i++){
+//     console.log(i)
+// }
+
+
+
+// Assignment
+
+
+// for (let i=0;i<=5;i=i+1){
+//     console.log(i);
+//     console.log(i+1);
+//     console.log(i-2);
+
+// }
+
+
+// let i;
+// for(i = 0; i<=5; i=i+1){
+//     console.log(i);
+// }
+// console.log(i+1);
+// console.log(i-2);
+
+
+
+// for(let i=5; i>0; --i){
+//     console.log(i);
+// }
+
+
+
+// for (let i=1; i<6; i++){
+//     console.log(i+5,i);
+// }
+
+
+
+// for(let j=5, k=1, l=2, m=3, n=4, x=5; j>0; ){
+//     console.log(x, x-1+k, x-2+l, x-3+m, x-4+n);
+//     j = j-1;
+//     k =0;
+//     l = 0;
+//     m =0;
+//     n =0;
+
+// }
+
+
+
+//WAP to print numbers from 1 to 10 using a for loop.
+// for(i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+
+//sum of digit numbers
+
+// let num=1234
+// let sum=0
+// for(;num>0;){
+//     rem=num%10;
+//     sum=sum+rem;
+//     num=parseInt(num/10)
+// }
+// console.log(sum)
+
+
+//products of digit number
+let num = 1234
+let mul=1
+
+for(;num>0;){
+    rem = num%10;
+    mul =mul*rem;
+    num =parseInt(num/10)
+}
+console.log(mul);
+
+
+
