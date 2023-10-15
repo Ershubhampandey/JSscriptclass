f82f50ed11116aa110279bc5e4d5e01c4a0acdfc
shubham <shubhampandeyup07@gmail.com>
2023-10-15 10:25:20 +0530

    string method to use here

---
+++ day10stringMethods/script.js
@@ -0,0 +1,16 @@
+// The length property returns the length of a string:
+
+// let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let length = text.length;
+// console.log(length);
+
+
+// Slice out a portion of a string from position 7 to position 13:
+
+// let text ="Apple, Banan, kiwi"
+// let part = text.slice(7,13);
+// console.log(part);
+
+let s1 =  "javascript from javatpoint indexof";
+let n = s1.indexOf("from");
+console.log(n);
