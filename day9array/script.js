d59b79ea628f2219f40f4eb20b161ddd7bc85bc1
shubham <shubhampandeyup07@gmail.com>
2023-10-15 09:04:58 +0530

    array code

---
+++ day9array/script.js
@@ -0,0 +1,27 @@
+// const prompt=require('prompt-sync')()
+// var a;
+// a=prompt("Enteer your name :")
+// console.log("a = ",a)
+
+
+// const prompt=require('prompt-sync')()
+// var name;
+// name =prompt("Enter your name :")
+// age=prompt("Enter your age : ")
+// console.log("Your name is ",name)
+// console.log("your age is",age)
+
+
+
+//how to get input by user
+
+
+const prompt=require('prompt-sync')()
+a=parseInt(prompt("Enter first number : "))
+b=parseInt(prompt("Enter  second number  : "))
+sum =a+b
+console.log("sum = ",sum)
+
+
+
+//start array class
